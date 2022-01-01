e61251d98b50eb8ab80a276f232379a4249de94a
xNovo3000 <fontana.ciro.2001@gmail.com>
2022-01-01 16:18:39 +0100

    Fixed and optimized IILogTarget

--- src/main/java/io/github/xnovo3000/sjll/logger/IILogTarget.java
+++ src/main/java/io/github/xnovo3000/sjll/logger/IILogTarget.java
@@ -28,35 +28,45 @@ class IILogTarget implements Runnable, AutoCloseable {
 		// Caches
 		final List<LogMessage> temporaryMessages = new ArrayList<>();
 		final StringBuilder messageBuilder = new StringBuilder();
-		// Endless loop
-		while (!shouldClose) {
-			// Destroy temporary messages
-			temporaryMessages.clear();
-			// Wait for the next message(s)
+		/* New code for target thread management */
+		// Case 1: messages.size() == 0 && !shouldClose -> wait
+		// Case 2: messages.size() == 0 && shouldClose -> break
+		// Case 3: messages.size() != 0 && !shouldClose -> write
+		// Case 4: messages.size() != 0 && shouldClose -> write
+		while (true) {
+			// Check the number of messages. If != 0 then write to the stream and flush
 			if (messages.size() == 0) {
+				// Only close if the messages are zero and shouldClose is true. Wait otherwise
+				if (shouldClose) {
+					break;
+				} else {
+					try {
+						wait();
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+				}
+			} else {
+				// Destroy temporary messages
+				temporaryMessages.clear();
+				// Get the messages
+				synchronized (messages) {
+					temporaryMessages.addAll(messages);
+					messages.clear();
+				}
+				// Write to the OutputStream and flush
 				try {
-					wait();
-				} catch (InterruptedException e) {
+					for (LogMessage logMessage : temporaryMessages) {
+						messageBuilder.setLength(0);
+						for (LogFormatter formatter : formatters) {
+							formatter.onFormat(messageBuilder, logMessage);
+						}
+						outputProvider.getOutputStream().write(messageBuilder.toString().getBytes());
+					}
+					outputProvider.getOutputStream().flush();
+				} catch (IOException e) {
 					e.printStackTrace();
 				}
-			}
-			// Get the messages
-			synchronized (messages) {
-				temporaryMessages.addAll(messages);
-				messages.clear();
-			}
-			// Write to the OutputStream and flush
-			try {
-				for (LogMessage logMessage : temporaryMessages) {
-					messageBuilder.setLength(0);
-					for (LogFormatter formatter : formatters) {
-						formatter.onFormat(messageBuilder, logMessage);
-					}
-					outputProvider.getOutputStream().write(messageBuilder.toString().getBytes());
-				}
-				outputProvider.getOutputStream().flush();
-			} catch (IOException e) {
-				e.printStackTrace();
 			}
 		}
 	}
