9111dc8232eee22589c048c69d8ae3812ed5f2c9
xNovo3000 <fontana.ciro.2001@gmail.com>
2022-01-04 17:41:34 +0100

    Error and tests changed

--- src/main/java/io/github/xnovo3000/sjll/exception/ConfigurationFileNotFoundException.java
+++ src/main/java/io/github/xnovo3000/sjll/exception/ConfigurationFileNotFoundException.java
@@ -1,11 +1,9 @@
 package io.github.xnovo3000.sjll.exception;
 
-import static io.github.xnovo3000.sjll.logger.LogFactory.CONFIGURATION_FILE_PATH;
-
 public class ConfigurationFileNotFoundException extends RuntimeException {
 	
-	public ConfigurationFileNotFoundException() {
-		super("The configuration file located at \"" + CONFIGURATION_FILE_PATH + "\" does not exists");
+	public ConfigurationFileNotFoundException(String filePath) {
+		super("The configuration file located at \"" + filePath + "\" does not exists");
 	}
 	
 }
--- src/test/java/io/github/xnovo3000/sjll/FirstTests.java
+++ src/test/java/io/github/xnovo3000/sjll/FirstTests.java
@@ -1,17 +1,57 @@
 package io.github.xnovo3000.sjll;
 
-import io.github.xnovo3000.sjll.logger.LogFactory;
 import io.github.xnovo3000.sjll.logger.Logger;
 import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 
 class FirstTests {
 
 	@Test
-	void firstTests() {
-		Logger logger = LogFactory.getLogger("MainLogger");
+	void firstTests() throws InterruptedException {
+		Logger logger = Logger.getLogger("MainLogger");
 		logger.d("Caller", "Custom message");
 		logger.i("Caller", "Custom message");
 		logger.e("Caller", "Custom message");
+		Thread.sleep(250);
+		logger.i("Caller", "Custom message");
+		logger.e("Caller", "Custom message");
+		Thread.sleep(500);
+		logger.i("Caller", "Custom message");
+		logger.e("Caller", "Custom message");
+		Thread.sleep(1000);
+	}
+	
+	@Test
+	void doMultithreadedTests() {
+		Logger logger = Logger.getLogger("MainLogger");
+		// Create the threads
+		List<Thread> ts = new ArrayList<>();
+		for (int i = 0; i < 20; i++) {
+			int finalI = i;
+			Thread t1 = new Thread(() -> {
+				for (int j = 0; j < 10; j++) {
+					try {
+						Thread.sleep(new Random().nextInt(200) + 200);
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+					logger.d("Runnable" + finalI, "Messaggio numero " + j);
+				}
+			});
+			t1.start();
+			ts.add(t1);
+		}
+		// Join
+		ts.forEach(t -> {
+			try {
+				t.join();
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		});
 	}
 	
 }
