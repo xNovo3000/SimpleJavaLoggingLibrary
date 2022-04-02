fe27d6f2489f4fc74a7f7e38de59599282e9356f
xNovo3000 <fontana.ciro.2001@gmail.com>
2022-04-02 19:18:38 +0200

    Implemented ILogger and ILogTarget

---
+++ src/main/java/io/github/xnovo3000/sjll/implementation/v1/ILogTarget.java
@@ -0,0 +1,85 @@
+package io.github.xnovo3000.sjll.implementation.v1;
+
+import io.github.xnovo3000.sjll.LogFormatter;
+import io.github.xnovo3000.sjll.LogTarget;
+import io.github.xnovo3000.sjll.data.LogMessage;
+import io.github.xnovo3000.sjll.outputprovider.OutputProvider;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+
+public class ILogTarget implements LogTarget {
+	
+	private final BlockingQueue<LogMessage> logMessageBlockingQueue;
+	private final List<LogFormatter> logFormatters;
+	private final OutputProvider outputProvider;
+	private final int minimumImportance;
+	
+	public ILogTarget(List<LogFormatter> logFormatters, OutputProvider outputProvider, int minimumImportance) {
+		this.logMessageBlockingQueue = new LinkedBlockingQueue<>();
+		this.logFormatters = logFormatters;
+		this.outputProvider = outputProvider;
+		this.minimumImportance = minimumImportance;
+	}
+	
+	@Override
+	public void run() {
+		// Cache the builder
+		final StringBuilder messageBuilder = new StringBuilder();
+		// For each message in the queue
+		while (!logMessageBlockingQueue.isEmpty()) {
+			// Try to get the message
+			LogMessage logMessage = null;
+			try {
+				logMessage = logMessageBlockingQueue.take();
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			// If the message exists
+			if (logMessage != null) {
+				// Reset messageBuilder
+				messageBuilder.setLength(0);
+				// Apply formatters in order
+				for (LogFormatter logFormatter : logFormatters) {
+					logFormatter.format(messageBuilder, logMessage);
+				}
+				// Write to the stream
+				try {
+					outputProvider.getOutputStream().write(messageBuilder.toString().getBytes(StandardCharsets.UTF_8));
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+		// Flush the stream
+		try {
+			outputProvider.getOutputStream().flush();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	@Override
+	public void enqueue(LogMessage logMessage) {
+		// If the importance level is right
+		if (logMessage.getLevel().getImportance() >= minimumImportance) {
+			// Try to insert the message
+			try {
+				logMessageBlockingQueue.put(logMessage);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	@Override
+	public void close() {
+		// This is called only when the application is killed
+		// Execute for the last time. Flush all pending messages
+		run();
+	}
+	
+}
---
+++ src/main/java/io/github/xnovo3000/sjll/implementation/v1/ILogger.java
@@ -0,0 +1,63 @@
+package io.github.xnovo3000.sjll.implementation.v1;
+
+import io.github.xnovo3000.sjll.LogTarget;
+import io.github.xnovo3000.sjll.Logger;
+import io.github.xnovo3000.sjll.data.Level;
+import io.github.xnovo3000.sjll.data.LogMessage;
+
+import java.util.List;
+import java.util.Objects;
+
+public class ILogger implements Logger {
+	
+	private final List<LogTarget> logTargets;
+	private final int minimumImportance;
+	private final String caller;
+	
+	public ILogger(List<LogTarget> logTargets, int minimumImportance, String caller) {
+		this.logTargets = logTargets;
+		this.minimumImportance = minimumImportance;
+		this.caller = caller;
+	}
+	
+	public ILogger copyWithCaller(String newCaller) {
+		return new ILogger(logTargets, minimumImportance, newCaller);
+	}
+	
+	@Override
+	public void d(Object object) {
+		if (Level.DEBUG.getImportance() >= minimumImportance) {
+			for (LogTarget logTarget : logTargets) {
+				logTarget.enqueue(new LogMessage(Level.DEBUG, caller, Objects.toString(object)));
+			}
+		}
+	}
+	
+	@Override
+	public void i(Object object) {
+		if (Level.INFO.getImportance() >= minimumImportance) {
+			for (LogTarget logTarget : logTargets) {
+				logTarget.enqueue(new LogMessage(Level.INFO, caller, Objects.toString(object)));
+			}
+		}
+	}
+	
+	@Override
+	public void w(Object object) {
+		if (Level.WARNING.getImportance() >= minimumImportance) {
+			for (LogTarget logTarget : logTargets) {
+				logTarget.enqueue(new LogMessage(Level.WARNING, caller, Objects.toString(object)));
+			}
+		}
+	}
+	
+	@Override
+	public void e(Object object) {
+		if (Level.ERROR.getImportance() >= minimumImportance) {
+			for (LogTarget logTarget : logTargets) {
+				logTarget.enqueue(new LogMessage(Level.ERROR, caller, Objects.toString(object)));
+			}
+		}
+	}
+	
+}
